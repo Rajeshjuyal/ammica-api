acc99baa23e3011b9b2054601b2c261aec07d03d
Ayush Tyagi <ayushtyagi2139@gmail.com>
2021-01-08 00:50:52 +0530

    subject modified

--- src/subject/subject.model.ts
+++ src/subject/subject.model.ts
@@ -2,14 +2,10 @@
 import * as mongoose from 'mongoose';
 export const SubjectSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  id: { type: String, required: true },
-  department: { type: String, required: true },
-  studentcode: { type: String, required: true },
+  subjectcode: { type: String, required: true },
 });
 
 export interface Subject {
-  id: string;
   name: string;
-  department: string;
-  studentcode: string;
+  subjectcode: string;
 }
