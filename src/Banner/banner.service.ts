462ffa5bbfc859511b4aaa06a52b71cb7ca933b3
Rajesh <juyalrajesh36@gmail.com>
2021-01-28 02:01:00 +0530

    Changes on Banner Module

--- src/Banner/banner.service.ts
+++ src/Banner/banner.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Banner } from './banner.model';
@@ -12,28 +12,42 @@ export class BannerService {
 
   public async create(bannerdata: Banner) {
     var banner1 = await this.bannerModel.create(bannerdata);
-    console.log(banner1);
-    return banner1;
+    return {
+      response_code: HttpStatus.OK,
+      response_data: banner1,
+    };
   }
 
   public async findAll() {
     var banners = await this.bannerModel.find();
-    return [...banners];
+    return {
+      response_code: HttpStatus.OK,
+      response_data: banners,
+    };
   }
 
   public async findOne(id: string) {
     var banners = await this.bannerModel.findById(id);
-    return banners;
+    return {
+      response_code: HttpStatus.OK,
+      response_data: banners,
+    };
   }
 
   public async update(id: string, bannerdata: Banner) {
     var banners = await this.bannerModel.findByIdAndUpdate(id, bannerdata);
-    return banners;
+    return {
+      response_code: HttpStatus.OK,
+      response_data: banners,
+    };
   }
 
   public async remove(id: string) {
     var banners = await this.bannerModel.findByIdAndDelete(id);
     banners.remove();
-    return banners;
+    return {
+      response_code: HttpStatus.OK,
+      response_data: 'Banner deleted',
+    };
   }
 }
