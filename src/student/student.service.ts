7ab645dcee1706efc823158a7071375da20ac259
Rajesh <juyalrajesh36@gmail.com>
2021-01-28 05:11:06 +0530

    multiple populate 2

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -40,7 +40,7 @@ export class StudentService {
     var periods = await this.periodModel
       .find({ section: student[0].section, Day: day })
       .populate('subject')
-      .populate('teacher');
+      .populate('user');
     return {
       response_code: HttpStatus.OK,
       response_data: periods,
