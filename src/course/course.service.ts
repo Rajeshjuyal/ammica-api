b835f86a9a6a47e641dd2b3827fb46e544312b65
Ayush Tyagi <ayushtyagi2139@gmail.com>
2021-01-29 00:56:48 +0530

    Response code added

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,5 +1,4 @@
-import { Injectable, Inject } from '@nestjs/common';
-
+import { Injectable, Inject, HttpStatus } from '@nestjs/common';
 import { Course } from './course.model';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
@@ -13,29 +12,43 @@ export class CourseService {
   public async create(course: Course) {
     console.log(' In function');
 
-    var collage1 = await this.courseModel.create(course);
-    console.log(collage1);
-    return collage1;
+    var Course = await this.courseModel.create(course);
+    return {
+      response_code: HttpStatus.OK,
+      response_data: Course,
+    };
   }
 
   public async findAll() {
     var courses = await this.courseModel.find();
-    return [...courses];
+    return {
+      response_code: HttpStatus.OK,
+      response_data: courses,
+    };
   }
 
   public async findOne(id: string) {
     var course = await this.courseModel.findById(id);
-    return course;
+    return {
+      response_code: HttpStatus.OK,
+      response_data: course,
+    };
   }
 
   public async update(id: string, coursedata: Course) {
     var course = await this.courseModel.findByIdAndUpdate(id, coursedata);
-    return course;
+    return {
+      response_code: HttpStatus.OK,
+      response_data: "Course Updated",
+    };
   }
 
   public async remove(id: string) {
     var course = await this.courseModel.findByIdAndDelete(id);
     course.remove();
-    return course;
+    return {
+      response_code: HttpStatus.OK,
+      response_data: "Course Deleted",
+    };
   }
 }
