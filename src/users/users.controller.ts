d01174dba9b62a2c0e0c9e92ec0feb5fbe5f4df7
Rajesh <juyalrajesh36@gmail.com>
2021-02-08 18:52:47 +0530

    count

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -239,6 +239,34 @@ export class UsersController {
   public getAllAdminCount(): Promise<CommonResponseModel> {
     return this.userService.getAdminCount();
   }
+  //get All usera list
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  @Get('all/school/count')
+  public getAllSchoolCount(): Promise<CommonResponseModel> {
+    return this.userService.getSchoolCount();
+  }
+  //get All usera list
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  @Get('all/teacher/count')
+  public getAllTeacherCount(): Promise<CommonResponseModel> {
+    return this.userService.getTeacherCount();
+  }
+  //get All usera list
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  @Get('all/student/count')
+  public getAllStudentCount(): Promise<CommonResponseModel> {
+    return this.userService.getstudentCount();
+  }
+  //get All usera list
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  @Get('all/parent/count')
+  public getAllParentCount(): Promise<CommonResponseModel> {
+    return this.userService.getParentCount();
+  }
 
   //get All usera list
   @UseGuards(AuthGuard('jwt'))
