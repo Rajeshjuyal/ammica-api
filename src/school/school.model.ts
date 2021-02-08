03cd1e295e781d90d4b9b5cab83692d7e780ab10
Rajesh <juyalrajesh36@gmail.com>
2021-02-08 17:13:18 +0530

    yo

--- src/school/school.model.ts
+++ src/school/school.model.ts
@@ -3,6 +3,17 @@ export const SchoolSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   name: { type: String, required: true },
   logo: { type: String, required: true },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  imageId: {
+    type: String,
+    required: true,
+  },
+  filePath: {
+    type: String,
+  },
   location: { type: String, required: true },
   board: { type: String, required: true },
   totalstudent: { type: String, required: true },
@@ -12,6 +23,9 @@ export interface School {
   user: string;
   _id: string;
   logo: string;
+  imageUrl:string
+  imageId: string;
+  filepath: string;
   name: string;
   location: string;
   board: string;
